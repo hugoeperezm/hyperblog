52b53c5882a33ccc7c0f1cac6c51c5c969f26253
Hugo Eduardo Perez <hugoeperezm@msn.com>
2022-03-16 17:20:09 -0500

    Se elimina archivos innecesarios y adicion de otros

---
+++ others.py
@@ -0,0 +1,7 @@
+def find_max(nums):
+    max_num = float("-inf") # smaller than all other numbers
+    for num in nums:
+        if num > max_num:
+        # (Fill in the missing line here)
+            max_num = num;
+    return max_num
